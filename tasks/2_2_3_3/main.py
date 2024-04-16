8f9650fbf1b018cd4e07cfd047199b0360c1b72b
Vladislav Voziyan <vladislav.voziyan@livion.fi>
2024-04-16 16:12:44 +0300

    fixed task 2.2.3.2

--- tasks/2_2_3_3/main.py
+++ tasks/2_2_3_3/main.py
@@ -2,16 +2,20 @@
 from pybricks.ev3devices import Motor
 from pybricks.parameters import Port
 from pybricks.robotics import DriveBase
+from pybricks.hubs import EV3Brick
 import time
 
 left_motor = Motor(Port.B)
 right_motor = Motor(Port.C)
 robot = DriveBase(left_motor, right_motor, wheel_diameter=54, axle_track=105)
+brick = EV3Brick()
+
 
 robot.settings(150)
 robot.straight(1000)
 robot.stop()
 time.sleep(2)
+brick.speaker.beep()
 robot.settings(50)
 robot.straight(-1000)
 
